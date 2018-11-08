add2c55df5310db7e72b8791e1af6ddaaa324d24
Karol Bemka <karolkarwia@gmail.com>
2018-11-08 23:28:32 +0100

    commit init

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,15 +1,95 @@
+import java.util.Scanner;
+
 public class TicTacToe {
     public static void main(String[] args) {
 
+        char[][] gameBoard = emptyBoard();
+        boolean shouldContinue = true;
+        boolean full = true;
+
+//        printBoard(emptyBoard()); nie potrzebne
+        printBoard(emptyBoard());
+
+        while (shouldContinue) {
+            while (full) {
+                int i1 = readFromConsole("pierwszy");
+                int j1 = readFromConsole("pierwszy");
+//                char[][] newGameBoard = gameBoard; chyba nie potrzebne
+                if (gameBoard[i1][j1] != '?') {
+                    System.out.println("To pole jest juz zajete, wybiez inne");
+                    printBoard(gameBoard);
+                } else {
+                    gameBoard[i1][j1] = 'X';
+                    full = false;
+                }
+            }
+            full = true;
+            printBoard(gameBoard);
+            if ((gameBoard[0][0] == gameBoard[0][1] && gameBoard[0][1] == gameBoard[0][2]) && gameBoard[0][2] == 'X' ||
+                (gameBoard[1][0] == gameBoard[1][1] && gameBoard[1][1] == gameBoard[1][2]) && gameBoard[1][2] == 'X' ||
+                (gameBoard[2][0] == gameBoard[2][1] && gameBoard[2][1] == gameBoard[2][2]) && gameBoard[2][2] == 'X' ||
+                (gameBoard[0][0] == gameBoard[1][0] && gameBoard[1][0] == gameBoard[2][0]) && gameBoard[2][0] == 'X' ||
+                (gameBoard[0][1] == gameBoard[1][1] && gameBoard[1][1] == gameBoard[2][1]) && gameBoard[2][1] == 'X' ||
+                (gameBoard[0][2] == gameBoard[1][2] && gameBoard[1][2] == gameBoard[2][2]) && gameBoard[2][2] == 'X' ||
+                (gameBoard[0][0] == gameBoard[1][1] && gameBoard[1][1] == gameBoard[2][2]) && gameBoard[2][2] == 'X' ||
+                (gameBoard[0][2] == gameBoard[1][1] && gameBoard[1][1] == gameBoard[2][0]) && gameBoard[2][0] == 'X') {
+                shouldContinue = false;
+                System.out.println("Wygral X");
+            }
+            if (shouldContinue) {
+                while (full) {
+                    int i2 = readFromConsole("drugi");
+                    int j2 = readFromConsole("drugi");
+                    if (gameBoard[i2][j2] != '?') {
+                        System.out.println("To pole jest juz zajete, wybiez inne");
+                        printBoard(gameBoard);
+                    } else {
+                        gameBoard[i2][j2] = 'O';
+                        full = false;
+                    }
+                }
+                full = true;
+                printBoard(gameBoard);
+                if ((gameBoard[0][0] == gameBoard[0][1] && gameBoard[0][1] == gameBoard[0][2]) && gameBoard[0][2] == 'O' ||
+                        (gameBoard[1][0] == gameBoard[1][1] && gameBoard[1][1] == gameBoard[1][2]) && gameBoard[1][2] == 'O' ||
+                        (gameBoard[2][0] == gameBoard[2][1] && gameBoard[2][1] == gameBoard[2][2]) && gameBoard[2][2] == 'O' ||
+                        (gameBoard[0][0] == gameBoard[1][0] && gameBoard[1][0] == gameBoard[2][0]) && gameBoard[2][0] == 'O' ||
+                        (gameBoard[0][1] == gameBoard[1][1] && gameBoard[1][1] == gameBoard[2][1]) && gameBoard[2][1] == 'O' ||
+                        (gameBoard[0][2] == gameBoard[1][2] && gameBoard[1][2] == gameBoard[2][2]) && gameBoard[2][2] == 'O' ||
+                        (gameBoard[0][0] == gameBoard[1][1] && gameBoard[1][1] == gameBoard[2][2]) && gameBoard[2][2] == 'O' ||
+                        (gameBoard[0][2] == gameBoard[1][1] && gameBoard[1][1] == gameBoard[2][0]) && gameBoard[2][0] == 'O') {
+                    shouldContinue = false;
+                    System.out.println("Wygral O");
+                }
+            }
+            }
+        }
+
+
+    static void printBoard(char[][] gameBoard) {
+        for (char[] x : gameBoard) {
+            for (char y : x) {
+                System.out.print(y + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static char[][] emptyBoard() {
         char[][] gameBoard = new char[3][3];
         for (int i = 0; i < gameBoard.length; i++) {
             for (int j = 0; j < gameBoard.length; j++) {
                 gameBoard[i][j] = '?';
             }
         }
-        for (int i = 0; i < gameBoard.length; i++) {
-                System.out.print(gameBoard[i][i] + " " + gameBoard[i][i+1] + " "  );
-            }
-        }
+        return gameBoard;
+    }
+
+    static int readFromConsole(String gracz) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Gracz " + gracz + " wybiera pole");
+        return scanner.nextInt();
     }
 }
+
+
